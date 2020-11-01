10d538e02980a7f7e60893a9a8ec6bfecf0b6bac
kayschmitt <kls2n4@mail.missouri.edu>
2020-11-01 13:23:40 -0600

    Edit Styling And Add Prettier

--- src/App.js
+++ src/App.js
@@ -1,65 +1,114 @@
-import React from 'react';
-import logo from './img/crumbdog.png';
-import './App.css';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import logo from "./img/crumbdog.png";
+import "./App.css";
+import Button from "@material-ui/core/Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import { faInstagram, faTiktok, faAmazon } from '@fortawesome/free-brands-svg-icons';
-import { faTshirt } from '@fortawesome/free-solid-svg-icons';
+import {
+  faInstagram,
+  faTiktok,
+  faAmazon,
+} from "@fortawesome/free-brands-svg-icons";
+import {
+  faEnvelope,
+  faMailBulk,
+  faTshirt,
+} from "@fortawesome/free-solid-svg-icons";
 
+import { withStyles } from "@material-ui/core/styles";
 
-import { withStyles } from '@material-ui/core/styles';
-
-
+const SocialButton = withStyles({
+  root: {
+    border: "none",
+    color: "white",
+    fontSize: "24px",
+    padding: "10px",
+    cursor: "pointer",
+    "&:hover": {
+      //background:'rgb(251, 229, 150, 1)',
+    },
+  },
+  label: {
+    textTransform: "capitalize",
+  },
+})(Button);
 
 const StyledButton = withStyles({
   root: {
-    background: 'rgb(251, 229, 150)',
-    borderRadius: 3,
-    border: '2px solid black',
-    color: 'black',
-    height: 48,
-    fontSize: '18px',
-    padding: '0 16px',
-    cursor: 'pointer',
-    '&:hover': {
-      background:'rgb(251, 229, 150, 1)',
-  }
+    background: "#E4432C",
+    borderRadius: 6,
+    border: "2px solid white",
+    color: "white",
+
+    fontSize: "24px",
+    padding: "18px 0",
+    cursor: "pointer",
+    "&:hover": {
+      background: "#E4432C",
+    },
   },
   label: {
-    textTransform: 'capitalize',
+    textTransform: "capitalize",
   },
 })(Button);
 
 function App() {
-
-  
   return (
     <div className="app">
+      <img src={logo} className="logo" />
 
-<img src={logo} className="logo"/>
+      <div className="grid2x2">
+        <div className="box">
+          <StyledButton
+            variant="outlined"
+            onClick={(e) => {
+              e.preventDefault();
+              window.location.href = "https://teespring.com/stores/crumb-dog";
+            }}
+          >
+            GET OFFICIAL MERCH
+          </StyledButton>
+        </div>
+      </div>
 
-<div className="grid2x2">
-<div className="box"><StyledButton variant="outlined" onClick={(e) => {
-      e.preventDefault();
-      window.location.href='https://teespring.com/stores/crumb-dog';
-      }}><FontAwesomeIcon icon={faTshirt} style={{paddingRight: "10px"}}/>Merch</StyledButton></div>
-  <div className="box">
-    <StyledButton variant="outlined" className='button' onClick={(e) => {
-      e.preventDefault();
-      window.location.href='https://www.instagram.com/crumb.dog/';
-      }}>
-      <FontAwesomeIcon icon={faInstagram} style={{paddingRight: "10px"}}/>Instagram </StyledButton>
-    </div>
-  <div className="box"><StyledButton variant="outlined"onClick={(e) => {
-      e.preventDefault();
-      window.location.href='https://www.tiktok.com/@crumb.dog';
-      }}><FontAwesomeIcon icon={faTiktok} style={{paddingRight: "10px"}}/>TikTok</StyledButton></div>
-  <div className="box"><StyledButton variant="outlined"onClick={(e) => {
-      e.preventDefault();
-      window.location.href='https://www.amazon.com/hz/wishlist/ls/36JW64UV7MDB6?ref_=wl_share';
-      }}><FontAwesomeIcon icon={faAmazon} style={{paddingRight: "10px"}}/>Wishlist</StyledButton></div>
-</div>
+      <div className="socialButtons">
+        <div className="headerButtons">
+          <SocialButton
+            variant="outlined"
+            className="button"
+            onClick={(e) => {
+              e.preventDefault();
+              window.location.href = "https://www.instagram.com/crumb.dog/";
+            }}
+          >
+            <FontAwesomeIcon icon={faInstagram} />
+          </SocialButton>
+
+          <SocialButton
+            variant="outlined"
+            onClick={(e) => {
+              e.preventDefault();
+              window.location.href = "https://www.tiktok.com/@crumb.dog";
+            }}
+          >
+            <FontAwesomeIcon icon={faTiktok} />
+          </SocialButton>
+
+          <SocialButton
+            variant="outlined"
+            onClick={(e) => {
+              e.preventDefault();
+              window.location.href = "mailto:crumbthegriff@gmail.com";
+            }}
+          >
+            <FontAwesomeIcon icon={faEnvelope} />
+          </SocialButton>
+        </div>
+
+        <span className="addressFooter">
+          Crumb Dog | P.O. Box 3769 | Kansas City, KS 66103
+        </span>
+      </div>
     </div>
   );
 }
